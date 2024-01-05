40e957157cc759960f95de62b9d05247f99cd8fd
Nitesh <niteshsoni598@gmail.com>
2024-01-05 17:35:43 +0530

    Hello Class is added in Config package

---
+++ src/main/java/com/SMS/config/Hello.java
@@ -0,0 +1,10 @@
+package com.SMS.config;
+
+public class Hello {
+    public static  void main (String [] args){
+        String name  = "Deeksha Singh Rajput";
+        long mobile = 6268723671l;
+         System.out.println("Name of Girl:"+name );
+        System.out.println("Mobile Number :"+mobile);
+    }
+}
